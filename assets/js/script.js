3c3aefe56ba237f579449604b5c097a37db972c3
JAY O DONOGHUE <jay.odonoghue@hotmail.com>
2022-12-05 19:27:29 +0000

    Add launchGame function to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,18 @@
+//Wait for the DOM to finish loading before running the game
+//Get the button elements and add event listeneres to them
+
+//document.addEventListener("DOMContentLoaded", function() {
+  //  let buttons = document.getElementsByTagName("button");
+//})
 
 const choices = ["the rock", "paper airplane", "scissors"];
 
-function launchGame() {
+const launchButton = document.getElementById("launch-game-btn")
+const introPage = document.querySelectorAll("#intro-page");
 
+launchButton.addEventListener("click", launchGame);
+
+
+function launchGame() {
+    introPage.classList.add("fade")
 }
