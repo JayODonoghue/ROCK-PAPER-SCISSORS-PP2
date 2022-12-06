dfc47a790a317393b1f8a105f2f4894c43bd622e
JAY O DONOGHUE <jay.odonoghue@hotmail.com>
2022-12-06 00:21:44 +0000

    Add possibleChoices function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,25 @@ document.addEventListener("DOMContentLoaded", function() {
    let buttons = document.getElementsByTagName("button");
 })
 
-const launchButton = document.getElementById("launch-game-btn")
+const launchButton = document.getElementById("launch-game-btn");
 const introPage = document.getElementById("intro-page");
-const gameArea = document.getElementById("game-area")
+const gameArea = document.getElementById("game-area");
+const choicesContainer = document.getElementById("choices-container")
+const possibleChoices = document.querySelectorAll(".choice");
+let userChoice
 
 /**Event Listener for Launch Game Button */
 launchButton.addEventListener("click", launchGame);
 
-
+possibleChoices[0].addEventListener("click", possibleChoice)
+possibleChoices[1].addEventListener("click", possibleChoice)
+possibleChoices[2].addEventListener("click", possibleChoice)
 
 function launchGame() {
-    introPage.style.display = 'none'
-    gameArea.style.display = 'block'
+    introPage.style.display = 'none';
+    gameArea.style.display = 'block';
+}
 
+function possibleChoice () {
+    choicesContainer.style.display = 'none';
 }
