1e75b753db8b555704a93d6fb1dd5bcff1d4ee01
JAY O DONOGHUE <jay.odonoghue@hotmail.com>
2022-12-07 00:15:57 +0000

    Add results functions to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,9 @@ const possibleChoices = document.querySelectorAll(".choice");
 const battleContainer = document.getElementById("battle-container");
 const playerChoiceDisplay = document.getElementsByClassName("player-choice");
 const computerChoiceDisplay = document.getElementsByClassName("computer-choice");
+let playerChoice
 let computerChoice
+
 
 /**Event Listener for Launch Game Button */
 launchButton.addEventListener("click", launchGame);
@@ -27,7 +29,7 @@ function launchGame() {
 }
 
 function possibleChoice (event) {
-    let playerChoice = event.target.className;
+    playerChoice = event.target.className;
     playerChoiceDisplay[0].innerHTML = playerChoice;
 
     choicesContainer.style.display = 'none';
@@ -39,16 +41,71 @@ function possibleChoice (event) {
 function createComputerChoice () {
     let randomNumber = Math.floor(Math.random() * possibleChoices.length) +1;
     
-    if (randomNumber === 1) {
-        computerChoice = "the-rock"
-    }
-    if (randomNumber === 2) {
-        computerChoice = "paper-airplane"
-    }
-    if (randomNumber === 3) {
-        computerChoice = "edward-scissorhands"
+    switch (randomNumber) {
+        case 1:
+            computerChoice = "the-rock";
+            break;
+        case 2:
+            computerChoice = "paper-airplane";
+            break;
+        case 3:
+            computerChoice = "edward-scissorhands";
     }
 
     computerChoiceDisplay[0].innerHTML = computerChoice;
+
+    createResults();
+}
+
+function createResults () {
+    let result
+    if (playerChoice === computerChoice) {
+        result = "IT'S A DRAW!";
+    }
+    if (playerChoice === "the-rock" && computerChoice === "paper-airplane") {
+        result = "YOU LOSE!";
+        addComputerScore ();
+    }
+    if (playerChoice === "the-rock" && computerChoice === "edward-scissorhands") {
+        result = "YOU WIN!";
+        addPlayerScore ();
+    }
+    if (playerChoice === "paper-airplane" && computerChoice === "edward-scissorhands") {
+        result = "YOU LOSE!";
+        addComputerScore ();
+    }
+    if (playerChoice === "paper-airplane" && computerChoice === "the-rock") {
+        result = "YOU WIN!";
+        addPlayerScore ();
+    }
+    if (playerChoice === "edward-scissorhands" && computerChoice === "the-rock") {
+        result = "YOU LOSE!";
+        addComputerScore ();
+    }
+    if (playerChoice === "edward-scissorhands" && computerChoice === "paper-airplane") {
+        result = "YOU WIN!";
+        addPlayerScore ();
+    }
+
+    
+    let resultMessage = document.createElement('div');
+    resultMessage.classList.add('result-message');
+    gameArea.appendChild(resultMessage);
+    gameArea.insertBefore(resultMessage, gameArea.children[3]);
+    
+    resultMessage.innerText = result;
+
+
+}
+
+function addPlayerScore () {
+        let oldScore = parseInt(document.getElementById("player-score").innerText);
+        document.getElementById("player-score").innerText = ++oldScore;
+    
+}
+
+function addComputerScore () {
+    let oldScore = parseInt(document.getElementById("computer-score").innerText);
+    document.getElementById("computer-score").innerText = ++oldScore;
 
 }
